73daaad868626ce70736c60a4b0f518acd391cf5
Julien Chastang <julien.c.chastang@gmail.com>
2011-12-02 16:07:52 -0700

    fixing paredit for OSX

--- config/bindings.el
+++ config/bindings.el
@@ -1,1 +1,8 @@
 ;; Place personal bindings here
+
+(eval-after-load "paredit"
+  '(progn
+     (define-key paredit-mode-map (kbd "M-[ 5 d")
+       'paredit-forward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "M-[ 5 c")
+       'paredit-forward-barf-sexp)))
