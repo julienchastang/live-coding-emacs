3b839c3ce188f20a35a584e8baf418cd3c59f448
Julien Chastang <julien.c.chastang@gmail.com>
2011-12-04 15:45:20 -0700

    orgmode customizations

--- config/bindings.el
+++ config/bindings.el
@@ -1,8 +1,25 @@
 ;; Place personal bindings here
 
+;;;;;;; paredit customization
+
 (eval-after-load "paredit"
   '(progn
      (define-key paredit-mode-map (kbd "M-[ 5 d")
        'paredit-forward-slurp-sexp)
      (define-key paredit-mode-map (kbd "M-[ 5 c")
        'paredit-forward-barf-sexp)))
+
+;;;;;;; org mode customization
+
+(setq org-todo-keywords
+             '((sequence "➜" "✈" "|" "✔" )))
+
+(setq org-todo-keyword-faces
+      (quote (("➜" :foreground "red"       :weight bold)
+	      ("✈" :foreground "goldenrod" :weight bold)
+	      ("✔" :foreground "forest green"))))
+
+(setq org-priority-faces
+      '((?A . (:foreground "red"))
+	(?B . (:foreground "#00BB00" ))
+	(?C . (:foreground "blue" ))))
