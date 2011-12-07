30f6158ea58e3cd7bfd237bcdd4671c0bcc5186d
Julien Chastang <julien.c.chastang@gmail.com>
2011-12-07 15:32:25 -0700

    adding paredit to slime repl

--- config/bindings.el
+++ config/bindings.el
@@ -9,6 +9,18 @@
      (define-key paredit-mode-map (kbd "M-[ 5 c")
        'paredit-forward-barf-sexp)))
 
+
+;; ParEdit and SLIME REPL
+
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+                (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
 ;;;;;;; org mode customization
 
 (setq org-todo-keywords
